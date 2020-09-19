0c3611cb45466c33e514831ee2530c07641174fb
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-09-19 12:59:58 -0400

    Developing common functions

--- src/test/java/com/hrms/utils/CommonMethods.java
+++ src/test/java/com/hrms/utils/CommonMethods.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.UnexpectedTagNameException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.hrms.testbase.PageInitializer;
@@ -79,35 +84,156 @@ public class CommonMethods extends PageInitializer {
 	}
 
 	/**
-	 * Method that will take a screenshot and store with name in specified location with .png extension
+	 * Method that will take a screenshot and store with name in specified location
+	 * with .png extension
+	 * 
 	 * @param fileName
 	 */
 	public static byte[] takeScreenshot(String fileName) {
 
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		byte[] bytes = ts.getScreenshotAs(OutputType.BYTES);
-		
+
 		File src = ts.getScreenshotAs(OutputType.FILE);
 		try {
-			FileUtils.copyFile(src, new File(Constants.SCREENSHOT_FILEPATH + fileName +getTimeStamp()+ ".png"));
+			FileUtils.copyFile(src, new File(Constants.SCREENSHOT_FILEPATH + fileName + getTimeStamp() + ".png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return bytes;
+
+	}
+
+	/**
+	 * This method will generate timeStamp
+	 * 
+	 * @return
+	 */
+	public static String getTimeStamp() {
+
+		Date date = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
+
+		return sdf.format(date);
+	}
+
+	/**
+	 * this method will click on checkbox or radio button by the given list of
+	 * webelements and the value
+	 * 
+	 * @param radioOrCheckBoxes
+	 * @param value
+	 */
+	public static void clickRadioOrCheckBox(List<WebElement> radioOrCheckBoxes, String value) {
+		String actualValue;
+		for (WebElement radioOrCheckbox : radioOrCheckBoxes) {
+			actualValue = radioOrCheckbox.getAttribute("value").trim();
+			if (radioOrCheckbox.isEnabled() && actualValue.equals(value)) {
+				jsClick(radioOrCheckbox);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * this method will select an option from the dropdown by the given webelement
+	 * and visible text value
+	 * 
+	 * @param dd
+	 * @param visibleTextOrValue
+	 */
+	public static void selectDDValue(WebElement dd, String visibleTextOrValue) {
+	try {
+		Select select = new Select(dd);
+		List<WebElement> options = select.getOptions();
+		for (WebElement option : options) {
+			if (option.getText().equals(visibleTextOrValue)) {
+				select.selectByVisibleText(visibleTextOrValue);
+				break;
+			}
+		}
+	} catch (UnexpectedTagNameException e) {
+		e.printStackTrace();
+	}
+		
+
+	}
+
+	/**
+	 * this method will select an option from the dropdown by the given webelement
+	 * and index
+	 * 
+	 * @param dd
+	 * @param visibleTextOrValue
+	 */
+	public static void selectDDValue(WebElement dd, int index) {
+		try {
+			Select select = new Select(dd);
+			List<WebElement> options = select.getOptions();
+
+			int size = options.size();
+
+			if (size > index) {
+				select.selectByIndex(index);
+			}
+		} catch (UnexpectedTagNameException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * this method will switch to a frame by the given frame weblement
+	 * @param iFrame
+	 */
+	public static void switchToFrame(WebElement iFrame) {
+		try {
+			driver.switchTo().frame(iFrame);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	/**
-	 * This method will generate timeStamp
-	 * @return
+	 * this method will switch to a frame by the given frame index
+	 * @param frameIndex
 	 */
-	public static String getTimeStamp() {
+	public static void switchToFrame(int frameIndex) {
+		try {
+			driver.switchTo().frame(frameIndex);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
 		
-		Date date=new Date();
-		
-		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
-		
-		return sdf.format(date);
 	}
+	
+	/**
+	 * this method will switch to a frame by the given frame nameOrId
+	 * @param nameOrId
+	 */
+	public static void switchToFrame(String nameOrId) {
+		try {
+			driver.switchTo().frame(nameOrId);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * this method will switch to a child window
+	 */
+	public static void switchToChildWindow() {
+		String mainWindow = driver.getWindowHandle();
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window : allWindows) {
+			if(!window.equals(mainWindow)) {
+				driver.switchTo().window(window);
+				break;
+			}
+		}
+	}
+
 }
--- src/test/resources/features/Dashboard.feature
+++ src/test/resources/features/Dashboard.feature
@@ -5,4 +5,3 @@ Feature: Dashboard Functionality
     When login with valid credentials
     Then verify the following dashboard tabs
       | Admin | PIM | Leave | Time | Recruitment | Performance | Dashboard | Directory |
-
